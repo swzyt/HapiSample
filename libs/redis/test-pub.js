42a5bcb4c26160ed343dbdf65177f7d6ecb72e91
苏伟 <915429829@qq.com>
2019-03-12 23:28:29 +0800

    redis 普通订阅 与 模式订阅

--- libs/redis/test-pub.js
+++ libs/redis/test-pub.js
@@ -1,18 +1,43 @@
+/**
+ * 请注意，redis发布和订阅客户端不允许为同一客户端
+ * 否则，将出现以下错误
+ * ReplyError: ERR only (P)SUBSCRIBE / (P)UNSUBSCRIBE / QUIT allowed in this context
+ * 字面意思是此上下文只允许有订阅和取消订阅功能。
+ * 解决方法：只要重新建立一个redis链接，专门用来做订阅和取消订阅功能。
+ */
+
 const client = require("../cache").client;
 client.select(1);//切换到数据库1
 
-//写入redis
+//写入redis，并发布消息至频道
 function insertRedis(key, value) {
     client.setAsync(key, value).then((result) => {
         //client将当前value发布到当前key频道,然后订阅这个频道的订阅者就会收到消息
         client.publish(key, value);
 
-        console.log(`发布信息了 ${key}: ${value}`);
+        console.log(`频道 ${key} 发布了消息：${value}`);
     })
 }
 
-for (let i = 0; i < 100; i++) {
+/******************************************************************** */
+//普通频道发布消息，subscribe方法订阅, unsubscribe方法取消订阅, message方法接收消息
+for (let i = 0; i < 5; i++) {
     setTimeout(() => {
-        insertRedis('test' + i, '' + i)
-    }, (i + 1) * 1000)
+        insertRedis('test-single', 'test-single' + i)
+    }, i * 1000)
 }
+
+/******************************************************************** */
+//模式频道发布消息，psubscribe方法订阅, punsubscribe方法取消订阅, pmessage方法接收消息
+for (let i = 5; i < 10; i++) {
+    setTimeout(() => {
+        insertRedis('test-pattern' + i, 'test-pattern' + i)
+    }, i * 1000)
+}
+
+/******************************************************************** */
+//15秒后关闭redis-pub
+setTimeout(() => {
+    console.log("15秒后关闭redis-pub")
+    client.quit();
+}, 15 * 1000)
--- libs/redis/test-sub.js
+++ libs/redis/test-sub.js
@@ -1,33 +1,71 @@
+/**
+ * 请注意，redis发布和订阅客户端不允许为同一客户端
+ * 否则，将出现以下错误
+ * ReplyError: ERR only (P)SUBSCRIBE / (P)UNSUBSCRIBE / QUIT allowed in this context
+ * 字面意思是此上下文只允许有订阅和取消订阅功能。
+ * 解决方法：只要重新建立一个redis链接，专门用来做订阅和取消订阅功能。
+ */
+
 const client = require("../cache").client;
 client.select(1);//切换到数据库1
 
 //客户端连接redis成功后执行回调
 client.on("ready", function () {
     //订阅消息
-    //目前测试，node下不支持正则订阅多个频道
-    //在命令行模式下支持
-    //client.subscribe("test");
-    //client.psubscribe("test.*");
-    //client.psubscribe(["test", "test0", "test1", "test2", "test3"]);
-    //client.PSUBSCRIBE(["test", "test0", "test1", "test2", "test3", "test*"]);
-    //client.PSUBSCRIBE(["test*"]);
 
-    for (let i = 0; i < 100; i++) {
-        client.subscribe("test" + i);
-    }
+    //普通订阅
+    client.subscribe("test-single");
+    //模式订阅，支持正则表达式
+    client.psubscribe("test-pattern*");
 });
 
-//监听订阅成功事件
+/******************************************************************** */
+//普通订阅-监听订阅成功事件
 client.on("subscribe", function (channel, count) {
-    console.log("client subscribed to " + channel + ", " + count + " total subscriptions");
+    console.log("普通订阅成功，频道：" + channel + ", 订阅数" + count);
 });
 
-//监听取消订阅事件
+//普通订阅-监听取消订阅事件
 client.on("unsubscribe", function (channel, count) {
-    console.log("client unsubscribed from" + channel + ", " + count + " total subscriptions")
+    console.log("普通订阅取消成功，频道：" + channel + ", 订阅数" + count);
 });
 
-//收到消息后执行回调，message是redis发布的消息
+//普通订阅收到消息，取消指定普通订阅频道
 client.on("message", function (channel, message) {
-    console.log(`收到信息了 ${channel}: ${message}`);
+    console.log(`普通订阅 收到信息， 频道：${channel}，消息 ${message}`);
 });
+
+//10秒后取消普通订阅
+setTimeout(() => {
+    client.unsubscribe("test-single");
+    client.unsubscribe("test-single-not-exist");//此为不存在频道
+}, 10 * 1000);
+
+/******************************************************************** */
+//模式订阅-监听订阅成功事件
+client.on("psubscribe", function (channel, count) {
+    console.log("模式订阅成功，频道：" + channel + ", 订阅数" + count);
+});
+
+//模式订阅-监听取消订阅事件
+client.on("punsubscribe", function (channel, count) {
+    console.log("模式订阅取消成功，频道：" + channel + ", 订阅数" + count);
+});
+
+//模式订阅收到消息
+client.on("pmessage", function (pattern, channel, message) {
+    console.log(`模式订阅 收到信息， 模式：${pattern}，频道：${channel}，消息 ${message}`);
+});
+
+//10秒后取消模式订阅，取消指定模式订阅频道
+setTimeout(() => {
+    client.punsubscribe("test-pattern*");
+    client.punsubscribe("test-pattern*-not-exist");//此为不存在频道
+}, 10 * 1000);
+
+/******************************************************************** */
+//15秒后关闭redis-sub
+setTimeout(() => {
+    console.log("15秒后关闭redis-sub")
+    client.quit();
+}, 15 * 1000)
